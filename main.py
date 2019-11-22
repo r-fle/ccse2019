f48c4f478fc6f937737f4fed6306359339744bb5
rifle <58030248+r-fle@users.noreply.github.com>
2019-11-22 15:49:25 +1100

    Create main.py for repl.it
    This is just a file to make the git repo seamlessly connect to repl.it.

---
+++ main.py
@@ -0,0 +1,21 @@
+# CCSE 2019 - Encoding? Encryption?
+print("PNRFNE IF KBERAPELCGVBA?")
+print("---")
+
+# How to start:
+# 1. Delete the '#' at the start of one of the levels below
+# 2. Click the 'run' button in the top bar!
+# 3. The output will be on the right hand pane.
+# 4. Edit the level file, and click run see your changes.
+
+#import level0
+#import level1
+#import level2
+#import level3
+#import level4
+#import level5
+#import level6
+
+# Use this file for trying out random things if you like.
+# Alternatively, you can run python commands directly
+# in the pane on the right. Try: 6 * 7 + 9 / 2
